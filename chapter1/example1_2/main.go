325204533ee48a1ae66d8d888dca3dc086a4581f
Christian Jodra <christian.jodra@ironchip.com>
2022-06-08 00:49:38 +0200

    using encoder instead marshal json object

---
+++ chapter1/example1_2/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type helloWorldResponse struct {
+	Message string `json:"message"`
+	Author  string `json:"-"`
+	Date    string `json:",omitempty"`
+	Id      int    `json:"id,string"`
+}
+
+func main() {
+	port := 8080
+
+	http.HandleFunc("/helloworld", helloWorldHandler)
+
+	log.Printf("Server starting on port %v\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	response := helloWorldResponse{Message: "Hello World!", Author: "Christian", Date: "05/07/2022", Id: 1}
+	encoder := json.NewEncoder(w)
+	encoder.Encode(&response)
+}
